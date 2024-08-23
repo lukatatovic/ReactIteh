591c8c67b10bf39211fb1e0a705764f66ed0b4fc
Luka <tatovicluka@gmail.com>
2024-08-24 00:07:34 +0200

    Created Router and pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
   return (
-    <div>SplitWise</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
