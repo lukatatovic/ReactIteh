01f9433f992907ba1c2659d94a495a1c79fcfb31
Luka <tatovicluka@gmail.com>
2024-08-24 02:52:36 +0200

    Updated Home page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import logo from '../assets/logo.png';
 
 const Home = () => {
+  const navigate = useNavigate(); 
   return (
-    <div>Home</div>
+    <div className='bg-light-green-cyan'>
+      <div className='flex justify-center items-center h-screen flex-col'>
+        <img
+          src={logo}
+          className='w-1/2 cursor-pointer'
+          onClick={() => navigate('/dashboard')}
+          alt='spliwise-logo'
+        />
+      </div>
+    </div>
   )
 }
 
