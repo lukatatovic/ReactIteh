b808301b244df7a45026f8574d37a3026fcf0432
TeodoraTrkulja <teodoratrkulja1@gmail.com>
2024-08-26 11:48:18 +0200

    Created Trips view.

--- client/src/components/dashboard/Trips.js
+++ client/src/components/dashboard/Trips.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { trips } from '../../utils/constants';
+import TripCard from './dashboard-reusable/TripCard';
 
 const Trips = () => {
-  return (
-    <div>Trips</div>
-  )
-}
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagesArray, setPagesArray] = useState([]);
 
-export default Trips
+  useEffect(() => {
+    let pages = [];
+    for (let i = 1; i <= Math.ceil(trips?.length / 8); i++) {
+      pages.push(i);
+    }
+    setPagesArray(pages);
+    setCurrentPage(1);
+  }, []);
+
+  return (
+    <div className='w-full'>
+      <div className='flex justify-between w-full'>
+        <h2 className='text-3xl font-bold p-2'>Trips</h2>
+        <div className='pagination'>
+          {pagesArray.map((page) => (
+            <p
+              key={page}
+              className={`pagination-box ${
+                page === currentPage && 'pagination-active'
+              }`}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className='flex flex-row flex-wrap gap-4'>
+        {trips.slice(currentPage * 8 - 8, currentPage * 8).map((trip, idx) => (
+          <TripCard key={idx} trip={trip} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Trips;
---
+++ client/src/components/dashboard/dashboard-reusable/TripCard.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import tripImg from '../../../assets/trip.jpg';
+
+const TripCard = ({ trip }) => {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const getImage = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.unsplash.com/photos/random?client_id=JAWLX5tqR51mrkwNozXCTrEHIQ4xTEI1CTnYtKBWGGA&query=${trip.destination}`
+        );
+        setImage(res.data.urls.full);
+      } catch (error) {
+        console.error('Reached the limit for API calls!');
+      }
+    };
+
+    getImage();
+  }, [trip.destination]);
+
+  return (
+    <div className='bg-white rounded-md w-96'>
+      <img
+        src={image || tripImg}
+        alt={'trip'}
+        className='rounded-md rounded-b-none w-96 h-60'
+      />
+      <div className='my-2 p-4'>
+        <h2 className='font-semibold'>{trip.destination}</h2>
+        <p className='text-sm mt-1'>
+          {trip.country}, {trip.city}
+        </p>
+        <p className='text-xs mt-1'>Start Date: {trip.startDate}</p>
+        <p className='text-xs mt-1'>End Date: {trip.endDate}</p>
+        <div className='flex flex-row gap-2 text-xs mt-2 text-neutral-500'>
+          <p>
+            <span>Total Cost: </span>
+            <span className='font-bold'>${trip.totalCost}</span>
+          </p>
+          <p>
+            <span>My Cost: </span>
+            <span className='font-bold'>${trip.myCost}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TripCard;
